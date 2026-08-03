ad72d16b317fc3a2ccf926eb7ae7d4c477aa08a7
403-22-Arya <54602@yupparaj.ac.th>
2026-08-03 13:20:43 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -6,7 +6,7 @@ price = st.number_input("กรอกราคาสินค้า (บาท):
 
 
  
-net_price = price - vat
+net_price = price-vat
 st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
 st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
 st.divider()
