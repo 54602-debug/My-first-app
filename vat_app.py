2861f71cfcf1bdef70f1ba80c0f556827c536cfd
403-22-Arya <54602@yupparaj.ac.th>
2026-08-03 13:33:22 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -1,4 +1,4 @@
- import streamlit as st
+import streamlit as st
 
 
 st.title("🛒แอปพลิเคชั่นคำนวณราคาสินค้ารวม VAT 7%")
@@ -7,7 +7,7 @@ st.title("🛒แอปพลิเคชั่นคำนวณราคาส
 net_price = price - vat
  
 price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
-st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
-st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
+st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.1.7}** บาท")
+st.header(f"• ราคาสุทธิ: {net_price:.1.7} บาท")
 st.divider()
-st.write("นางสาวดีใจ ยิ้มแย้ม เลขที่ 5  ม.4/5")
+st.write("นางสาวอารยา  กันยา เลขที่ 22 ม.4/3")
